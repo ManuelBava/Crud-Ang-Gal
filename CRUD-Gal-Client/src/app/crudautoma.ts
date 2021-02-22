0a520ed7d1a44a242b2ad4dc3149846782b9b8c0
leoIapp <leoiappelli01@gmail.com>
2021-02-22 23:18:09 +0100

    impl. automa, state

---
+++ CRUD-Gal-Client/src/app/crudautoma.ts
@@ -0,0 +1,64 @@
+import { AddEvent, AnnullaEvent, Automabile, ConfermaEvent, ModificaEvent, RicercaEvent, SelezionaEvent, State } from "./state";
+
+export class CrudAutoma implements State {
+    stato: State;
+
+    next(e: Event) {
+        this.stato.next(e);
+    }
+}
+
+export class Ricerca implements State {
+    next(e: Event, a: CrudAutoma) {
+        if (e instanceof AddEvent) {
+            a.stato = new Ricerca();
+        } else if (e instanceof SelezionaEvent) {
+            a.stato = new Visualizza();
+        } else if (e instanceof AddEvent) {
+            a.stato = new Aggiungi();
+        }
+    }
+}
+
+export class Aggiungi implements State {
+    next(e: Event, a: CrudAutoma) {
+        if (e instanceof ConfermaEvent) {
+            a.stato = new Visualizza();
+        } else if (e instanceof AnnullaEvent) {
+            a.stato = new Ricerca();
+        }
+    }
+}
+
+export class Visualizza implements State {
+    next(e: Event, a: CrudAutoma) {
+        if (e instanceof ModificaEvent) {
+            a.stato = new Ricerca();
+        } else if (e instanceof RicercaEvent) {
+            a.stato = new Ricerca();
+        } else if (e instanceof AddEvent) {
+            a.stato = new Aggiungi();
+        }
+    }
+}
+
+export class Modifica implements State {
+    next(e: Event, a: CrudAutoma) {
+        if (e instanceof AnnullaEvent) {
+            a.stato = new Visualizza();
+        } else if (e instanceof ConfermaEvent) {
+            a.stato = new Visualizza();
+        }
+    }
+}
+
+export class Rimuovi implements State {
+    next(e: Event, a: CrudAutoma) {
+        if (e instanceof AnnullaEvent) {
+            a.stato = new Visualizza();
+        } else if (e instanceof ConfermaEvent) {
+            a.stato = new Ricerca();
+        }
+    }
+}
+
---
+++ CRUD-Gal-Client/src/app/state.ts
@@ -0,0 +1,40 @@
+import { CrudAutoma } from "./CrudAutoma";
+
+export interface State {
+     next(e : Event, a? : CrudAutoma)
+}
+
+export interface Event{
+
+}
+export class AddEvent implements Event{
+
+}
+
+export class AnnullaEvent implements Event{
+
+}
+
+export class ConfermaEvent implements Event{
+
+}
+
+export class ModificaEvent implements Event{
+
+}
+
+export class RicercaEvent implements Event{
+
+}
+
+export class RimuoviEvent implements Event{
+
+}
+
+export class SelezionaEvent implements Event{
+
+}
+
+export interface Automabile {
+
+}
